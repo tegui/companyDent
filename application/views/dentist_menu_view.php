bb5328124809e90a7c57241fdff0b5df5d727426
David Tegui <david@Davids-MacBook-Pro.local>
2017-05-29 15:17:09 -0500

    Clinical history linked (without view)

--- application/views/dentist_menu_view.php
+++ application/views/dentist_menu_view.php
@@ -6,52 +6,56 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>Company Dent</title>
-    <!-- Bootstrap Core CSS -->
-    <link href="http://localhost/companyDent/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="http://localhost/companyDent/css/full-slider.css" rel="stylesheet">
+  <title>Company Dent</title>
+  <!-- Bootstrap Core CSS -->
+  <link href="http://localhost/companyDent/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="http://localhost/companyDent/css/full-slider.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-           <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href= "<?= base_url('dentist')?>">Company Dent - Odontologo</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+  <!-- Navigation -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href= "<?= base_url('dentist')?>">Company Dent - Odontologo</a>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
 
-                    <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Citas<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="<?= base_url('dentist/list_appointments')?>">Consultar citas</a></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gestión de Citas<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="<?= base_url('dentist/list_appointments')?>">Consultar citas</a></li>
+            </ul>
+          </li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gestión de historias clínicas<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="<?= base_url('dentist/medicalHistory')?>">Crear historia clínica</a></li>
+            </ul>
+          </li>
 
-
-              </ul>
-            </li>
-
-             <li>
-                        <a href="<?= base_url('login/get_out') ?>"> Cerrar Sesion</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+          <li>
+            <a href="<?= base_url('login/get_out') ?>"> Cerrar Sesion</a>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+  </nav>
